251d54ba3aacc9b2701bb6602d04f258f597eb29
HarshithUK <harshithuk.acharya@gmail.com>
2016-10-01 17:41:23 +0530

    updated the implementation details

--- utils.h
+++ utils.h
@@ -8,6 +8,33 @@
      using FSA based RegEx matcher
     *Accepts the file name to be pre-processed
 */
-int find_length_of_text (char* file_name);
+//int find_length_of_text (char* file_name);
+int* rabin_karp(char* t, char* p)
+{
 
+}
+int* knuth_morris_pratt(char* t, char* p)
+{
+
+}
+int* suffix_trees(char* t, char* p)
+{
+
+}
+int find_length_of_text(FILE* fp)
+{
+
+}
+int* find_pattern(char* p, char* text, int* (*func_algo)(char*, char*))
+{
+}
+void build_cross_index(FILE* fp, int* (*func_algo)(char*, char*))
+{
+}
+char* find_maximal_palindromes(int s, int[] text)
+{
+}
+void print_stats()
+{
+}
 #endif
