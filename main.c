0fc2b1a18f3fe404f71fce096ea1e56d7003f4c3
HarshithUK <harshithuk.acharya@gmail.com>
2016-10-17 22:44:12 +0530

    Added an option to choose patterns/indices for tr

--- main.c
+++ main.c
@@ -29,9 +29,39 @@ int main()
             case 2 : printf("\nEnter a pattern: ");
                      char pat[15];
                      scanf(" %[^\n]s",pat);
-                     printf("\nEnter a text range: ");
-                     scanf("%d%d",&tr[0],&tr[1]);
                      int c=0;
+                     printf("\nHow do you want to input your text range?\n");
+                     printf("\n1.By two indices");
+                     printf("\n2.By two distinct patterns");
+                     int c=0;
+                     do
+                     {
+                        printf("\nType a valid option:");
+                        scanf("%d",&c);
+                     }
+                     while(c!=1 && c!=2);
+                     if(c==1)
+                     {
+                        printf("\nEnter a text range indices: ");
+                        scanf("%d%d",&tr[0],&tr[1]);
+                     }
+                     else
+                     {
+                        char p1[50];
+                        char p2[50];
+                        printf("\nEnter two distinct patterns: ");
+                        scanf("%s%s",p1,p2);
+                        int pos1 = first_occurence(p1);
+                        int pos2 = first_occurence(p2);
+                        if(pos2 < pos1)
+                        {
+                            int tempos = pos1;
+                            pos1 = pos2;
+                            pos2 = tempos;
+                        }
+                        tr[0] = pos1;
+                        tr[1] = pos2;
+                     }
                      printf("\nWhich algorithm do you need?\n");
                      printf("\n1.Rabin-Karp");
                      printf("\n2.Knuth-Morris-Pratt");
